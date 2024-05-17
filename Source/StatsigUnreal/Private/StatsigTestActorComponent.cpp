441f3db0431b4b4800f23a46d1c2281522ef796c
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-05-17 13:40:00 -0700

    feat: support Layer|Experiment::GetGroupName()

--- Source/StatsigUnreal/Private/StatsigTestActorComponent.cpp
+++ Source/StatsigUnreal/Private/StatsigTestActorComponent.cpp
@@ -28,26 +28,36 @@ void QueryStatsig() {
   auto values = experiment.GetValues();
   auto value = values->GetStringField(TEXT("a_string"));
   Log(FString::Format(
-          TEXT("an_experiment.a_string: {0} ({1})"),
-          {value, experiment.GetEvaluationDetails().reason}),
-      FColor::Blue);
+          TEXT("an_experiment.a_string -- {0} -- {1} -- {2}"),
+          {
+            value.IsEmpty() ? "NULL" : value,
+            experiment.GetEvaluationDetails().reason,
+            experiment.GetGroupName().value_or("NULL")
+          }),
+      FColor::Green);
 
   auto not_an_experiment = client.GetExperiment("not_an_experiment");
   auto not_a_value =
       not_an_experiment.GetValues()->GetStringField(TEXT("not_real"));
   Log(FString::Format(
-          TEXT("not_an_experiment.not_real: {0} ({1})"),
-          {not_a_value,
-           not_an_experiment.GetEvaluationDetails().reason}),
-      FColor::Blue);
+          TEXT("not_an_experiment.not_real -- {0} -- {1} -- {2}"),
+          {
+              not_a_value.IsEmpty() ? "NULL" : not_a_value,
+              not_an_experiment.GetEvaluationDetails().reason,
+              not_an_experiment.GetGroupName().value_or("NULL"),
+          }),
+      FColor::Green);
 
   auto layer = client.GetLayer("a_layer");
   auto param = layer.GetValue("a_string");
 
-  Log(FString::Format(TEXT("a_layer.a_string: {0} ({1})"),
-                      {param.has_value() ? param.value()->AsString() : TEXT(""),
-                       layer.GetEvaluationDetails().reason}),
-      FColor::Blue);
+  Log(FString::Format(TEXT("a_layer.a_string -- {0} -- {1} -- {2}"),
+                      {
+                        param.has_value() ? param.value()->AsString() : TEXT(""),
+                       layer.GetEvaluationDetails().reason,
+                        layer.GetGroupName().value_or("NULL"),
+                      }),
+      FColor::Green);
 
   client.LogEvent({"my_custom_event", "some_value_😎"});
 
---
+++ Source/StatsigUnreal/Private/statsig_compat/primitives/file_path.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include "Containers/UnrealString.h"
+
+namespace statsig {
+
+typedef FString FilePath;
+
+}
